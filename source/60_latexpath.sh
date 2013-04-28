d9cfc71fc5442464216bde3f8a7a723000102a97
John Hogenmiller <john@yourtech.us>
2013-04-28 14:39:04 -0400

    added closing endif tag

--- source/60_latexpath.sh
+++ source/60_latexpath.sh
@@ -1,3 +1,5 @@
 # Always use color output for `ls`                                             
 if [[ "$OSTYPE" =~ ^darwin ]]; then                                            
     export PATH=$PATH:/usr/local/texlive/2012/bin/x86_64-darwin/
+endif
+
