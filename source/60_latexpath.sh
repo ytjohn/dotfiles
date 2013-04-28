182ccee4b546116789da50e021c4a7da7887f599
John Hogenmiller <john@yourtech.us>
2013-04-28 13:59:29 -0400

    add path to latex on mac systems

--- source/60_latexpath.sh
+++ source/60_latexpath.sh
@@ -1,1 +1,3 @@
-export PATH=$PATH:/usr/local/texlive/2012/bin/x86_64-darwin/
+# Always use color output for `ls`                                             
+if [[ "$OSTYPE" =~ ^darwin ]]; then                                            
+    export PATH=$PATH:/usr/local/texlive/2012/bin/x86_64-darwin/
